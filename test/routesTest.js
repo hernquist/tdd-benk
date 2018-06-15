8939f24aab70ba80003247d92540e15c73954ab7
David Hernquist <hernquistdavid@gmail.com>
2018-06-15 10:53:31 -0400

    solution

--- test/routesTest.js
+++ test/routesTest.js
@@ -3,15 +3,17 @@ const expect = require("chai").expect;
 const request = require("supertest");
 const api = request("http://localhost:3001");
 
-describe("OWNERS", function() {
-  describe("GET ALL", function() {
-    it("tests first and last from owners", function(done) {
+
+describe("Owners", function() {
+  describe("get all", function() {
+    it("tests first and last from all owners", function(done) {
       api
         .get("/")
         .set("Accept", "application/json")
         .expect(200) //tests the status of the call
         .end(function(err, res) {
           if (err) return done(err);
+          
           let owners = res.body.map(owner => owner);
           let first = owners[0] 
           let last = owners[owners.length-1];
